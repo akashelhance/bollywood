26c773971c6aaa365190588b6bc0c8f8fe4c0530
akash elhance <akashelhance@gmail.com>
2024-07-21 23:58:44 +0530

    image quality

--- components/CelebCardComponent.js
+++ components/CelebCardComponent.js
@@ -47,7 +47,7 @@ const celebrities= await fetchData()
         // style={{
         //   objectFit: 'contain'
         // }}
-        
+        unoptimized
          quality={100}
          width={100}
          height={100}
